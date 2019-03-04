7457fb449945bd9cce08e6387db39b28cc17d560
Roxolana <roxolanapavlyk@gmail.com>
2019-03-04 16:42:42 +0100

    add error handler

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -3,14 +3,26 @@ import axios from 'axios'
 
 
 export default class Delete extends Component {
-    RecipeDelete=e=>{
-        e.preventDefault();
-        axios.delete('http://127.0.0.1:8000/recipes/'+ this.props.id + '/')
-        .then(res=>{
-          console.log(res.data)
-        })
-        .then(window.location.reload())
+    // RecipeDelete = e => {
+    //     e.preventDefault();
+    //     axios.delete('http://127.0.0.1:8000/recipes/'+ this.props.id + '/')
+    //     .then(res=>{
+    //       console.log(res.data)
+    //     })
+    //     .then(window.location.reload())
+    // }
+    RecipeDelete = async (e) => {
+    e.preventDefault()
+    try{
+  const res = await axios.delete('http://127.0.0.1:8000/recipes/' + this.props.id + '/')   
+  if(!res.ok){
+    throw Error(res.statusText)
+  }
+} catch(error){
+      console.log(error)
     }
+      window.location.reload()
+  }
 
   render() {
     return (
